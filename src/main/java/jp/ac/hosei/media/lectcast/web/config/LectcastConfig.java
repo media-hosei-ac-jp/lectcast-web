1f059d66cf06be42e5bc3b695117631c367f699d
HATAKEYAMA, Hisashi <hisashi.hatakeyama.33@hosei.ac.jp>
2020-04-22 17:21:52 +0900

    Added a custom dialect for line break

---
+++ src/main/java/jp/ac/hosei/media/lectcast/web/config/LectcastConfig.java
@@ -0,0 +1,15 @@
+package jp.ac.hosei.media.lectcast.web.config;
+
+import jp.ac.hosei.media.thymeleaf.dialect.HoseiDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LectcastConfig {
+
+    @Bean
+    public HoseiDialect hoseiDialect() {
+        return new HoseiDialect();
+    }
+
+}
---
+++ src/main/java/jp/ac/hosei/media/thymeleaf/dialect/HoseiDialect.java
@@ -0,0 +1,28 @@
+package jp.ac.hosei.media.thymeleaf.dialect;
+
+import jp.ac.hosei.media.thymeleaf.processor.BreakLineProcessor;
+import org.thymeleaf.dialect.AbstractProcessorDialect;
+import org.thymeleaf.processor.IProcessor;
+import org.thymeleaf.standard.StandardDialect;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HoseiDialect extends AbstractProcessorDialect {
+
+    private static final String NAME = "customized dialect";
+    private static final String PREFIX = "hosei";
+
+    public HoseiDialect() {
+        super(NAME, PREFIX, StandardDialect.PROCESSOR_PRECEDENCE);
+    }
+
+    @Override
+    public Set<IProcessor> getProcessors(String dialectPrefix) {
+        Set<IProcessor> proccessors = new HashSet<>();
+
+        proccessors.add(new BreakLineProcessor(dialectPrefix, getDialectProcessorPrecedence()));
+
+        return proccessors;
+    }
+}
